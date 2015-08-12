f2a940c76c420e6680758c69152123747ea435e2
Dominic Cerquetti <binary1230@gmail.com>
2015-08-12 19:58:44 -0400

    more dire warning about test data

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from generator import BarcodeNumberGenerator
 if __name__ == '__main__':
     generator = BarcodeNumberGenerator("badges.yaml")
 
-    # todo: don't harcode these, obviously.
+    # TEST DATA
+    # Anyone using this for production, make sure you change this. Obviously.
+    # You will want to burn these particular numbers if you're anywhere near production servers.
+    # And you will want to guard that secret_key with your goddamned life.
     generator.secret_key = bytes('JK65*&5Ba|','ascii') # must be 10 bytes
     generator.salt = 87
     generator.event_id = 255
